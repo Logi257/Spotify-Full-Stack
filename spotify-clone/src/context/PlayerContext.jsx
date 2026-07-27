cf9a7c280450c08e2390c187973edc85c81c26fe
Logesh Paramalai <125880059+Logi257@users.noreply.github.com>
2026-07-27 14:53:35 +0530

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
   const seekBar = useRef();
   const seekBg = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "https://spotify-full-stack-yhzt.onrender.com";
 
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
