18ff6a74276c4a0f957864bc8935e539a38cdcb1
Logesh Paramalai <125880059+Logi257@users.noreply.github.com>
2026-07-28 17:38:35 +0530

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from "./pages/ListAlbum";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 
-export const url = 'https://spotify-backend-2n7e.onrender.com/'
+export const url = 'https://spotify-backend-2n7e.onrender.com'
 
 const App = () => {
   return (
